e1279e4b9355ddcaf5ea67ee8b24adfad88a2678
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-12 19:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required.
+let d1=new Date();
 let a=document.getElementById('output');
 a.innerHTML=` <tr><td colspan='2'>Loading...</td></tr>`;
 const p1=new Promise((a,b)=>{
@@ -18,11 +19,12 @@ const p3=new Promise((a,b)=>{
 });
 let array=[p1,p2,p3];
 Promise.all(array).then((d)=>{
-	
+	let d2=new Date();
+	let final=d2.getSeconds()-d1.getSeconds()+(d2.getMilliseconds()-d1.getMilliseconds())/1000;
 	a.innerHTML=``;
 	a.innerHTML=`<tr><td>Promise 1</td><td>1.500</td></tr>
 	<tr><td>Promise 2</td><td>1.200</td></tr>
 	<tr><td>Promise 3</td><td>1.950</td></tr>
-	<tr><td>Total</td><td>2.950</td></tr>
+	<tr><td>Total</td><td>${final}</td></tr>
 	`;
 });
