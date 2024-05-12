5eade63d5ba0b1be8ee2acc76b1c6f6e43e11e93
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-12 19:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,25 +3,25 @@
 const p1=new Promise((a,b)=>{
 	setTimeout(()=>{
 		a('resolved');
-	},1500);
+	},2500);
 });
 const p2=new Promise((a,b)=>{
 	setTimeout(()=>{
 		a('resolved');
-	},2000);
+	},2900);
 });
 const p3=new Promise((a,b)=>{
 	setTimeout(()=>{
 		a('resolved');
-	},2500);
+	},2950);
 });
 let array=[p1,p2,p3];
 Promise.all(array).then((d)=>{
 	let a=document.getElementById('output');
 	a.innerHTML=``;
-	a.innerHTML=`<tr><td>Promise 1<td><td>1.500<td></tr>
-	<tr><td>Promise 2<td><td>2.000<td></tr>
-	<tr><td>Promise 3<td><td>2.500<td></tr>
-	<tr><td>Total<td><td>6.000<td></tr>
+	a.innerHTML=`<tr><td>Promise 1<td><td>2500<td></tr>
+	<tr><td>Promise 2<td><td>2.900<td></tr>
+	<tr><td>Promise 3<td><td>2.950<td></tr>
+	<tr><td>Total<td><td>2.950<td></tr>
 	`;
 });
