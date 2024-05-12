6d5326273b57b7ffe2d9ed962ce0eb5ab24ea2fa
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-05-12 19:23:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
-
+let a=document.getElementById('output');
+a.innerHTML=` <tr><td colspan='2'>Loading...</td></tr>`;
 const p1=new Promise((a,b)=>{
 	setTimeout(()=>{
 		a('resolved');
@@ -17,11 +18,11 @@ const p3=new Promise((a,b)=>{
 });
 let array=[p1,p2,p3];
 Promise.all(array).then((d)=>{
-	let a=document.getElementById('output');
+	
 	a.innerHTML=``;
-	a.innerHTML=`<tr><td>Promise 1<td><td>2500<td></tr>
-	<tr><td>Promise 2<td><td>2.900<td></tr>
-	<tr><td>Promise 3<td><td>2.950<td></tr>
-	<tr><td>Total<td><td>2.950<td></tr>
+	a.innerHTML=`<tr><td>Promise 1</td><td>2500</td></tr>
+	<tr><td>Promise 2</td><td>2.900</td></tr>
+	<tr><td>Promise 3</td><td>2.950</td></tr>
+	<tr><td>Total</td><td>2.950</td></tr>
 	`;
 });
